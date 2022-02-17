bd3711a18b5f57bc4ca0f9066b2a924a40182965
Csaba Benko <csaba.benko@gmail.com>
2022-02-17 02:08:58 +0100

    Added room selection control to hourly room event display.

--- ChatRoom/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ChatRoom/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,26 +1,60 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
-export class FetchDataComponent {
+export class FetchDataComponent implements OnInit {
   public forecasts: HourlyChatEvent[] = [];
+  public chatRooms: ChatRoom[] = [];
+  contactForm: FormGroup;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  constructor(private fb: FormBuilder, private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    this.contactForm = this.fb.group({
+      room: [null]
+    });
+    
+    http.get<ChatRoom[]>(baseUrl + 'ChatRoom').subscribe(result => {
+      this.chatRooms = result;
+    });
+
     http.get<HourlyChatEvent[]>(baseUrl + 'ChatRoom/2/hourlyevents').subscribe(result => {
-      this.forecasts = result;
+      this.processRoomEvents(result);
+    }, error => console.error(error));
+  }
 
-      var prevHourPart = "";
-      this.forecasts.forEach(element => {
-        if (element.hourPart == prevHourPart) {
-          element.sameHourAsPrevious = true;
-        } else {
-          element.sameHourAsPrevious = false;
-          prevHourPart = element.hourPart
-        }
-      });
+  private processRoomEvents(result: HourlyChatEvent[]) {
+    this.forecasts = result;
+
+    var prevHourPart = "";
+    this.forecasts.forEach(element => {
+      if (element.hourPart == prevHourPart) {
+        element.sameHourAsPrevious = true;
+      } else {
+        element.sameHourAsPrevious = false;
+        prevHourPart = element.hourPart;
+      }
+    });
+  }
+
+  ngOnInit() {
+ 
+    this.contactForm = this.fb.group({
+      room: [2]
+    });
+
+    this.contactForm.get("room")!.valueChanges
+    .subscribe(f=> {
+      this.onChatRoomSelectionChange(f);
+    });
+  }
+
+  onChatRoomSelectionChange(roomId: number){
+    console.log(roomId);
+    this.http.get<HourlyChatEvent[]>(this.baseUrl + `ChatRoom/${roomId}/hourlyevents`).subscribe(result => {
+      this.processRoomEvents(result);
     }, error => console.error(error));
   }
 }
@@ -40,4 +74,9 @@ interface HourlyChatEvent {
   targetUser: number;
   hourPart: string;
   sameHourAsPrevious: boolean;
+}
+
+interface ChatRoom {
+  chatRoomId: number;
+  name: string;
 }
